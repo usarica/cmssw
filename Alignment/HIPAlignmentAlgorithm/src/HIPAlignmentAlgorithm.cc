ea27ee316480b46fb6d0065fc62f3debd2e593f2
Ulascan Sarica <ulascan90@gmail.com>
2017-07-21 18:37:47 +0200

    19584 review 3, to_string instead of stringstream

--- Alignment/HIPAlignmentAlgorithm/src/HIPAlignmentAlgorithm.cc
+++ Alignment/HIPAlignmentAlgorithm/src/HIPAlignmentAlgorithm.cc
@@ -1292,10 +1292,7 @@ void HIPAlignmentAlgorithm::collector(void){
     for (int ijob=1; ijob<=theCollectorNJobs; ijob++){
       edm::LogInfo("Alignment") << "@SUB=HIPAlignmentAlgorithm::collector" << "Pre-reading uservar for job " << ijob;
 
-      std::stringstream ss;
-      std::string str;
-      ss << ijob;
-      ss >> str;
+      std::string str = std::to_string(ijob);
       std::string uvfile = theCollectorPath+"/job"+str+"/"+suvarfilecore;
 
       std::vector<AlignmentUserVariables*> uvarvec = HIPIO.readHIPUserVariables(theAlignables, uvfile.c_str(), theIteration, ioerr);
@@ -1336,10 +1333,7 @@ void HIPAlignmentAlgorithm::collector(void){
   for (int ijob=1; ijob<=theCollectorNJobs; ijob++){
     edm::LogInfo("Alignment") << "@SUB=HIPAlignmentAlgorithm::collector" << "Reading uservar for job " << ijob;
 
-    std::stringstream ss;
-    std::string str;
-    ss << ijob;
-    ss >> str;
+    std::string str = std::to_string(ijob);
     std::string uvfile = theCollectorPath+"/job"+str+"/"+suvarfilecore;
 
     std::vector<AlignmentUserVariables*> uvarvec = HIPIO.readHIPUserVariables(theAlignables, uvfile.c_str(), theIteration, ioerr);
